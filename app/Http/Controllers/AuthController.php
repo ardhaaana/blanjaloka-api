0bdfc080e798a88c7ebf2226024a81b1bfd4e8ce
ardhaaana <ardhanrcw@gmail.com>
2021-10-27 20:22:53 +0700

    fix login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+
 
 class AuthController extends Controller
 {
@@ -42,6 +45,37 @@ class AuthController extends Controller
             ], 400);
         }
 
+    }
+    public function login(Request $request)
+    {
+        $var = Str::random(40);
+        $email_customer = $request->input('email_customer');
+        $password = $request->input('password');
+
+        $login = Customer::where('email_customer', $email_customer)->first();
+
+        if (Hash::check($password, $login->password)){
+            $apiToken = base64_encode($var);
+
+            $login->update([
+                'api_token' => $apiToken
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Login sukses',
+                'data' => [
+                    'registrasi' => $login,
+                    'api_token' => $apiToken
+                ]
+                ],201);
+        }else{
+            return response()->json([
+                'success' => false,
+                'message' => 'Login gagal',
+                'data' => ''
+                ]);
+        }
     }
 
 }
