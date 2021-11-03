968778d366e8c0d91d0436961c9b6243acec4d05
fikrifayadhi <90893692+fikrifayadhi@users.noreply.github.com>
2021-11-03 16:11:16 +0700

    fix web

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ $router->post('/register', 'AuthController@register');
 $router->post('/login', 'AuthController@login');
 
 
-$router->group(['prefix' => 'api','middleware' => 'auth'], function() use ($router) {
+$router->group(['prefix' => 'api', 'middleware' => 'auth'], function () use ($router) {
 
     $router->get('produk', ['uses' => 'ProdukController@index']);
     $router->post('produk', ['uses' => 'ProdukController@create']);
@@ -34,16 +34,16 @@ $router->group(['prefix' => 'api','middleware' => 'auth'], function() use ($rout
     $router->get('produk/{kode_produk}', ['uses' => 'ProdukController@show']);
     $router->put('produk/{kode_produk}', ['uses' =>  'ProdukController@update']);
 
-    $router->delete('produk/{kode_produk}', ['uses' =>'ProdukController@destroy']);
+    $router->delete('produk/{kode_produk}', ['uses' => 'ProdukController@destroy']);
 
-   $router->post('jamoperasional',['uses' => 'JamoperasionalpasarController@create'] );
-    $router->get('jamoperasional', ['uses' =>  'JamoperasionalpasarController@index']);
+    $router->post('jamoperasional', ['uses' => 'JamOperasionalPasarController@create']);
+    $router->get('jamoperasional', ['uses' =>  'JamOperasionalPasarController@index']);
 
     $router->get('jamoperasional/{id_toko}', ['uses' =>  'JamoperasionalpasarController@show']);
     $router->put('jamoperasional/{id_toko}', ['uses' =>  'JamoperasionalpasarController@update']);
     $router->delete('jamoperasional/{id_toko}', ['uses' =>  'JamoperasionalpasarController@destroy']);
 
-    $router->post('pendaftaran',['uses' => 'PendaftaranController@create'] );
+    $router->post('pendaftaran', ['uses' => 'PendaftaranController@create']);
     $router->get('pendaftaran', ['uses' =>  'PendaftaranController@index']);
 
     $router->get('pendaftaran/{id_pendaftaran}', ['uses' =>  'PendaftaranController@show']);
@@ -51,13 +51,18 @@ $router->group(['prefix' => 'api','middleware' => 'auth'], function() use ($rout
     $router->delete('pendaftaran/{id_pendaftaran}', ['uses' =>  'PendaftaranController@destroy']);
 
 
-    $router->post('role',['uses' => 'RoleController@create'] );
+    $router->post('role', ['uses' => 'RoleController@create']);
     $router->get('role', ['uses' =>  'RoleController@index']);
 
     $router->get('role/{id_role}', ['uses' =>  'RoleController@show']);
     $router->put('role/{id_role}', ['uses' =>  'RoleController@update']);
     $router->delete('role/{id_role}', ['uses' =>  'RoleController@destroy']);
 
-});
 
- 
+    $router->post('pedagang', ['uses' => 'PedagangController@create']);
+    $router->get('pedagang', ['uses' =>  'PedagangController@index']);
+
+    $router->get('pedagang/{id_pedagang}', ['uses' =>  'PedagangController@show']);
+    $router->put('pedagang/{id_pedagang}', ['uses' =>  'PedagangController@update']);
+    $router->delete('pedagang/{id_pedagang}', ['uses' =>  'PedagangController@destroy']);
+});
