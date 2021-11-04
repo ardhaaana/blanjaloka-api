207af893ab2cfcbbed047f494089b7f9262e19c4
fikrifayadhi <90893692+fikrifayadhi@users.noreply.github.com>
2021-11-04 15:07:45 +0700

    login dan register pengelola pasar

---
+++ app/Models/PengelolaPasar.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PengelolaPasar extends Model
+{
+    protected $primaryKey = 'id_pengelola';
+    public $table = "pengelola_pasar";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nama_pengelola', 'alamat_pengelola', 'nomor_telepon', 'email', 'username', 'password', 'id_role', 'token_pp'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    // protected $hidden = [
+    //     'password', 'token'
+    // ];
+}
