f7ba5042d72d7ca4fd0d290e55325d17adbd53da
Ardhana Ramadhan <86752366+ardhaaana@users.noreply.github.com>
2021-11-08 14:47:00 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -102,12 +102,4 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->put('toko/{id_toko}', ['uses' =>  'TokoController@update']);
     $router->delete('toko/{id_toko}', ['uses' =>  'TokoController@destroy']);
 
-    
-    $router->get('shopcart', ['uses' => 'ShopController@index']);
-    $router->post('shopcart', ['uses' => 'ShopController@create']);
-
-    $router->get('shopcart/{id_produk}', ['uses' => 'ShopController@show']);
-    $router->put('shopcart/{id_produk}', ['uses' =>  'ShopController@update']);
-
-    $router->delete('shopcart/{id_produk}', ['uses' => 'ShopController@destroy']);
 });
