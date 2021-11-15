2a090896c1fb953c44bfb4ba45d5c8f48a6428a9
Ardhana Ramadhan <86752366+ardhaaana@users.noreply.github.com>
2021-11-15 19:37:23 +0700

    Update AuthCustomerController.php
    Perubahan Profile Customer

--- app/Http/Controllers/AuthCustomerController.php
+++ app/Http/Controllers/AuthCustomerController.php
@@ -103,9 +103,72 @@ class AuthCustomerController extends Controller
     {
       
         $this->validate($request, [
-            'nama_customer' => 'required|min:5',
-            'nomor_telepon' => 'required',
+            'nama_customer' => 'required|min:5'
+        ]);
+
+      if ($request->has('email_customer')) {
+            $update = Customer::query()->find($id_customer)->update(
+                [
+                'nama_customer' => $request->input('nama_customer'),
+                'alamat_customer' => $request->input( 'alamat_customer'),
+                'tanggal_lahir' => $request->input( 'tanggal_lahir')
+                ]
+            );
+        } else {
+            $update = Customer::query()->find($id_customer)->update(
+                [
+                'nama_customer' => $request->input('nama_customer'),
+                'alamat_customer' => $request->input( 'alamat_customer'),
+                'tanggal_lahir' => $request->input( 'tanggal_lahir')
+                ]
+            );
+        }
+
+        
+        if (!$update) {
+            return response()->json(['error' => 'unknown error'], 500);
+        }
+
+         return response()->json([
+            'message' => 'Edit Profile update!',
+            'code' => 200
+        ]);
+    }
+    public function emailupdate(Request $request, $id_customer)
+    {
+      
+        $this->validate($request, [
             'email_customer' => 'required|email',
+        ]);
+
+      if ($request->has('email_customer')) {
+            $update = Customer::query()->find($id_customer)->update(
+                [
+                'email_customer' => $request->input('email_customer'),
+                ]
+            );
+        } else {
+            $update = Customer::query()->find($id_customer)->update(
+                [
+                'alamat_customer' => $request->input( 'alamat_customer')
+                ]
+            );
+        }
+
+        
+        if (!$update) {
+            return response()->json(['error' => 'unknown error'], 500);
+        }
+
+         return response()->json([
+            'message' => 'Edit Email update!',
+            'code' => 200
+        ]);
+    }
+    public function passwordupdate(Request $request, $id_customer)
+    {
+      
+        $this->validate($request, [
             'username' => 'required|min:5',
             'password' => 'required|min:8'
         ]);
@@ -113,11 +176,6 @@ class AuthCustomerController extends Controller
       if ($request->has('email_customer')) {
             $update = Customer::query()->find($id_customer)->update(
                 [
-                'nama_customer' => $request->input('nama_customer'),
-                'nomor_telepon' => $request->input('nomor_telepon'),
-                'email_customer' => $request->input('email_customer'),
-                'alamat_customer' => $request->input( 'alamat_customer'),
-                'tanggal_lahir' => $request->input( 'tanggal_lahir'),
                 'username' => $request->input('username'),
                 'password' => Hash::make($request->input('password')),
                 ]
@@ -125,10 +183,6 @@ class AuthCustomerController extends Controller
         } else {
             $update = Customer::query()->find($id_customer)->update(
                 [
-                'nama_customer' => $request->input('nama_customer'),
-                'nomor_telepon' => $request->input('nomor_telepon'),
-                'alamat_customer' => $request->input( 'alamat_customer'),
-                'tanggal_lahir' => $request->input( 'tanggal_lahir'),
                 'username' => $request->input('username'),
                 'password' => Hash::make($request->input('password'))
                 ]
@@ -141,7 +195,38 @@ class AuthCustomerController extends Controller
         }
 
          return response()->json([
-            'message' => 'Edit Profile update!',
+            'message' => 'Edit Password update!',
+            'code' => 200
+        ]);
+    }
+    public function teleponupdate(Request $request, $id_customer)
+    {
+      
+        $this->validate($request, [
+            'nomor_telepon' => 'required'
+        ]);
+
+      if ($request->has('email_customer')) {
+            $update = Customer::query()->find($id_customer)->update(
+                [
+                'nomor_telepon' => $request->input('nomor_telepon')
+                ]
+            );
+        } else {
+            $update = Customer::query()->find($id_customer)->update(
+                [
+                'nomor_telepon' => $request->input('nomor_telepon')
+                ]
+            );
+        }
+
+        
+        if (!$update) {
+            return response()->json(['error' => 'unknown error'], 500);
+        }
+
+         return response()->json([
+            'message' => 'Edit Nomor telepon update!',
             'code' => 200
         ]);
     }
