1a73b8fd6b0b80d5b158213288b1fcee53ec356d
Ardhana Ramadhan <86752366+ardhaaana@users.noreply.github.com>
2021-11-18 00:22:15 +0700

    Add files via upload
    Favorit Produk

---
+++ app/Models/FavoritProduk.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FavoritProduk extends Model
+{
+    
+    public $table = "favorit_produk";
+    protected $primaryKey = 'id';
+    
+    protected $fillable = [
+        'id_produk','id_customer'
+    ];
+    
+     protected $hidden = ["created_at", "updated_at"];
+
+    public function produk(){
+        return $this->belongsTo(Produk::class, 'id_produk');
+    }
+    
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class, 'id_customer');
+    }
+
+}
