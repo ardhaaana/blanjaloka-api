1e68d5c6ef600124559e6850b7f8f39c3c589fb1
Ardhikarestu <61740978+ardhikarestuyoviyanto@users.noreply.github.com>
2021-11-25 22:27:31 +0700

    Update FavoritProdukController.php

--- app/Http/Controllers/FavoritProdukController.php
+++ app/Http/Controllers/FavoritProdukController.php
@@ -7,78 +7,113 @@ use App\Models\Customer;
 use App\Models\FavoritProduk;
 use App\Models\Produk;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
-class FavoritProdukController extends Controller
-{
-    public function index()
-    {
+class FavoritProdukController extends Controller{
 
-       $favoritproduk = favoritproduk::with('produk')->get();
+    public function __construct(){
+        
+        $this->middleware('auth');
 
-        if (!$favoritproduk) {
-            return response()->json(['success' => 0, 'message' => 'Favorit tidak ditemukan'], 404);
-        }
-
-        return response()->json([
-            'success' => 1,
-            'message' => 'Menampilkan Favorit Produk',
-            'Favorit Produk' => $favoritproduk
-        ], 200);
     }
 
-    public function create(Request $request)
-    {
-        $validator = Validator::make($request->only('id_produk'), [
-            'id_produk' => "required"
+
+    # Tambah Favorite
+    public function create(Request $request){
+
+        $this->validate($request,[
+            'id_produk' => 'required|numeric',
+            'id_customer' => 'required|numeric'
         ]);
 
-        if ($validator->fails()) {
-            return response()->json(['success' => 0, 'message' => 'Required or incorrect fields', 'errors' => $validator->errors()], 500);
+        # Error jika user menambahkan produk yang sama ke favorit 
+        if(count(FavoritProduk::where('id_customer', $request->input('id_customer'))->where('id_produk', $request->input('id_produk'))->get()) > 0){
+
+            $data = FavoritProduk::select('id', 'nama_produk')
+                    ->join('produk', 'produk.id_produk', '=', 'favorit_produk.id_produk')
+                    ->where('favorit_produk.id_customer', $request->input('id_customer'))
+                    ->where('favorit_produk.id_produk', $request->input('id_produk'))->get();
+
+            return response()->json([
+                'success' => 0,
+                'message' => 'Produk Ini Telah Ada di List Produk Favorit',
+                'data' => $data
+            ], 200);
         }
 
-        $produk = Produk::find($request->input('id_produk'));
+        # Handler Jika id Produk Tidak Ditemukan
+        if(count(Produk::where('id_produk', $request->input('id_produk'))->get()) == 0){
 
-        if (!$produk) {
-            return response()->json(['success' => 0, 'message' => 'Produk tidak ditemukan'], 404);
+            return response()->json([
+                'success' => 0,
+                'message' => 'id produk tidak ditemukan'
+            ], 500);
+
         }
 
-        $favoritproduk = new FavoritProduk();
-        $favoritproduk->id_produk = $request->input('id_produk');
-        $favoritproduk->id_customer = $request->input('id_customer');
-        $favoritproduk->save();
+        # Handler Jika id customer Tidak Ditemukan
+        if(count(Customer::where('id_customer', $request->input('id_customer'))->get()) == 0){
 
-        $favoritproduk = FavoritProduk::with('produk')->where('id_customer')->where('id_produk', $request->input('id_produk'))->first();
+            return response()->json([
+                'success' => 0,
+                'message' => 'id customer tidak ditemukan'
+            ], 500);
+
+        }
+
+        # Input Favorit
+        $data = [
+            'id_customer' => $request->input('id_customer'),
+            'id_produk' => $request->input('id_produk')
+        ];
+
+        FavoritProduk::create($data);
+
+        $favoritproduk = FavoritProduk::select('favorit_produk.id', 'nama_produk')
+                        ->join('produk', 'produk.id_produk', '=', 'favorit_produk.id_produk')
+                        ->where('favorit_produk.id_customer', $request->input('id_customer'))
+                        ->where('favorit_produk.id_produk', $request->input('id_produk'))->get();
 
         return response()->json([
             'success' => 1,
             'message' => 'Produk ditambah di favorit',
-            'Favorit Produk' => $produk
+            'data' => $favoritproduk
         ], 200);
+
     }
 
-    public function show($id)
-    {
-        $favoritproduk = favoritproduk::with('produk')->find($id);
+    # Nampilin data favorit by customer
+    public function show($id_customer){
 
-        if (!$favoritproduk) {
-            return response()->json(['success' => 0, 'message' => 'Favorit tidak ditemukan'], 404);
-        }
+        // id disini yang ditampilin adalah id favorit_produk
+        $listprodukfavorit = Produk::select('id', 'nama_produk', 'satuan', 'harga_jual', 'stok_saat_ini', 'deskripsi', 'foto_produk', 'status_produk', 'id_pedagang')
+                            ->join('favorit_produk', 'favorit_produk.id_produk', '=', 'produk.id_produk')
+                            ->where('favorit_produk.id_customer', $id_customer)->get();
 
-        return response()->json(['item' => $favoritproduk], 200);
+        $customers = Customer::select('id_customer', 'nama_customer')->where('id_customer', $id_customer)->get();
+
+        return response()->json([
+            'customer' => $customers,
+            'listfavoritproduk' => $listprodukfavorit
+        ], 200);
+        
     }
 
-    public function destroy($id)
-    {
+    # Nampilin data favorit by customer
+    public function destroy($id){
+
         $favoritproduk = FavoritProduk::with('produk')->find($id);
 
         if (!$favoritproduk) {
-            return response()->json(['success' => 0, 'message' => 'Favorit tidak ditemukan'], 404);
+            return response()->json([
+                'success' => 0, 'message' => 'Favorit tidak ditemukan'
+            ], 404);
         }
 
         $favoritproduk->delete();
-
-        return response()->json(['success' => 1, 'message' => 'Produk Berhasil Dihapus Di Favorit'], 200);
+        return response()->json([
+            'success' => 1, 
+            'message' => 'Produk Berhasil Dihapus Di Favorit'
+        ], 200);
     }
 }
 
