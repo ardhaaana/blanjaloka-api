a1b619a91e4d1e9627e55e5e0ff4c84bf43b0358
Ardhana Ramadhan <86752366+ardhaaana@users.noreply.github.com>
2021-11-26 16:06:24 +0700

    Update KategoriProdukController.php

--- app/Http/Controllers/KategoriProdukController.php
+++ app/Http/Controllers/KategoriProdukController.php
@@ -45,60 +45,6 @@ class KategoriProdukController extends Controller
         return response()->json($kategori);
     }
 
-    public function show($id_kategori)
-    {
-        $kategori = KategoriProduk::find($id_kategori);
-
-         if (empty($kategori)) {
-            return response()->json(['error' => 'Kategori Produk Tidak Ditemukan'], 402);
-        }
-
-        return response()->json($kategori);
-    }
-    public function produkshow(Request $request, $id_kategori)
-    {
-        $kategori = KategoriProduk::find($id_kategori);
-        $produk = Produk::all();
-         $query = $request->query('query');
-
-        if (empty($query)) {
-            return response()->json(['error' => 'Query not specified!'], 400);
-        }
-
-        $fulltext = $request->query('fulltext', 'false');
-        $sortBy = $request->query('sort_by', 'nama_produk.asc');
-        $sorts = explode('.', $sortBy);
-
-
-        if ($fulltext == 'true') {
-            $produk = Produk::query()
-                ->whereRaw("MATCH(nama_produk,deskripsi) AGAINST(? IN BOOLEAN MODE)", array($query))
-                ->orderBy($sorts[0], $sorts[1])
-                ->get();
-             return response()->json(['message' => 'Success',
-            'Kategori' => $kategori,
-            'Produk' => $produk
-        ]);
-        }
-
-        $produk = Produk::query()
-            ->where('nama_produk', 'like', '%' . $query . '%')
-            ->orWhere('deskripsi', 'like', '%' . $query . '%')
-            ->orderBy($sorts[0], $sorts[1])
-            ->get();
-
-        if (empty($kategori)) {
-            return response()->json(['error' => 'Kategori Produk Tidak Ditemukan'], 402);
-        }
-
-        return response()->json(['message' => 'Success',
-        'Kategori' => $kategori,
-        'Produk' => $produk
-    ]);
-    
-    }
-
-
     public function update(Request $request, $id_kategori)
     {
         $kategori = KategoriProduk::find($id_kategori);
