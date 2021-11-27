c721ca3d0ac0fd1a0f4489c8a3cf5a4642e23612
Ardhana Ramadhan <86752366+ardhaaana@users.noreply.github.com>
2021-11-27 16:22:53 +0700

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,11 +8,11 @@ class Controller extends BaseController
 {
     protected function respondWithToken($token)
     {
-        return response()->json([
-            'Message' => 'Login Success',
-            'token' => $token,
-            'token_type' => 'bearer',
-            // 'expires_in' => Auth::factory()->getTTL() * 60
-        ], 200);
+//         return response()->json([
+//             'Message' => 'Login Success',
+//             'token' => $token,
+//             'token_type' => 'bearer',
+//             // 'expires_in' => Auth::factory()->getTTL() * 60
+//         ], 200);
     }
 }
