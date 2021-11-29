4553507fd3ec78bcfb953393a772244afc9353b1
Ardhikarestu <61740978+ardhikarestuyoviyanto@users.noreply.github.com>
2021-11-29 22:48:18 +0700

    Update KeranjangProdukController.php

--- app/Http/Controllers/KeranjangProdukController.php
+++ app/Http/Controllers/KeranjangProdukController.php
@@ -17,34 +17,30 @@ class KeranjangProdukController extends Controller{
     public function create(Request $request){
 
         $this->validate($request, [
-            'id_customer' => 'required|numeric',
+            // 'id_customer' => 'required|numeric',
             'id_produk' => 'required|numeric',
             'jumlah_produk' => 'required|numeric'
         ]);
 
-        if(count(KeranjangProduk::where('id_produk', $request->input('id_produk'))->where('id_customer', $request->input('id_customer'))->get()) > 0){
+        if(count(KeranjangProduk::where('id_produk', $request->input('id_produk'))->where('id_customer', $request->session()->get('id_customer'))->get()) > 0){
 
             return response([
-                'code' => 200,
-                'success' => false,
                 'message' => 'Produk Sudah Ada di Keranjang'
-            ]);
+            ], 200);
 
         }else{
 
             $keranjang = KeranjangProduk::create([
-                'id_customer' => $request->input('id_customer'),
+                'id_customer' => $request->session()->get('id_customer'),
                 'id_produk' => $request->input('id_produk'),
                 'jumlah_produk' => $request->input('jumlah_produk')
             ]);
     
             if($keranjang){
                 return response()->json([
-                    'code' => 200,
-                    'success' => true,
                     'message' => 'Produk ditambahkan ke keranjang',
                     'data' => $keranjang
-                ]);
+                ], 200);
             }
 
         }
@@ -52,16 +48,16 @@ class KeranjangProdukController extends Controller{
     }
 
     # Tampilkan data Keranjang
-    public function show($id_customer){
+    public function show(Request $request){
 
         $isikeranjang = DB::table('keranjang_produk')->select('keranjang_produk.id_keranjang', 'keranjang_produk.id_produk', 'produk.nama_produk', 'keranjang_produk.jumlah_produk', 'produk.harga_jual', DB::raw("keranjang_produk.`jumlah_produk` * produk.`harga_jual` AS total"))
                         ->join('produk', 'keranjang_produk.id_produk', '=', 'produk.id_produk')
-                        ->where('keranjang_produk.id_customer', $id_customer)
+                        ->where('keranjang_produk.id_customer', $request->session()->get('id_customer'))
                         ->get();
                         
         $subtotal = KeranjangProduk::select(DB::raw("keranjang_produk.`jumlah_produk` * produk.`harga_jual` AS subtotal"))
                     ->join('produk', 'keranjang_produk.id_produk', '=', 'produk.id_produk')
-                    ->where('id_customer', $id_customer)
+                    ->where('id_customer', $request->session()->get('id_customer'))
                     ->get();
 
         $_subtotal = 0;
@@ -73,16 +69,13 @@ class KeranjangProdukController extends Controller{
         if($isikeranjang && $subtotal){
         
             return response()->json([
-                'code' => 200,
-                'success' => true,
-                'message' => 'Menampilkan Isi Keranjang',
                 'keranjang' => $isikeranjang,
                 'sub_total' => $_subtotal
-            ]);
+            ], 200);
         
         }else{
 
-            return response()->json(['code' => 500,'success' => false, 'error' => 'unknown error']);
+            return response()->json(['error' => 'unknown error'], 500);
         
         }
 
@@ -100,7 +93,7 @@ class KeranjangProdukController extends Controller{
         ]);
 
         if(!$query){
-            return response()->json(['code' => 500,'success' => false, 'error' => 'unknown error']);
+            return response()->json(['error' => 'unknown error'], 500);
         }
 
         $data = DB::table('keranjang_produk')->select('keranjang_produk.id_keranjang', 'produk.nama_produk', 'keranjang_produk.jumlah_produk', 'produk.harga_jual', DB::raw("keranjang_produk.`jumlah_produk` * produk.`harga_jual` AS total"))
@@ -109,11 +102,9 @@ class KeranjangProdukController extends Controller{
                 ->get();
 
         return response()->json([
-            'code' => 200,
-            'success' => true, 
             'message' => "Keranjang Berhasil Diperbaruhi",
             'data' => $data
-        ]);
+        ], 200);
         
     }
 
@@ -124,15 +115,11 @@ class KeranjangProdukController extends Controller{
 
         if(!$query){
             return response()->json([
-                'code' => 500,
-                'success' => false, 
                 'message' => 'unknown error'
-            ]);
+            ], 500);
         }
 
         return response([
-            'code' => 200,
-            'success' => true, 
             'message' => "Keranjang Berhasil Dihapus"
         ]);
 
